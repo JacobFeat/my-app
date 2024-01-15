27b75a2352839a7605e9023e33257b0a1062391e
JacobFeat <jakubk198@gmail.com>
2024-01-15 21:27:34 +0100

    initial

--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -2,14 +2,17 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
-import { ArticlesService } from './articles.service';
+import { ArticlesService } from '../articles.service';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-articles',
   standalone: true,
   imports: [CommonModule, RouterOutlet, HttpClientModule, RouterModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  templateUrl: './articles.component.html',
+  styleUrl: './articles.component.scss'
 })
-export class AppComponent {
+export class ArticlesComponent {
+  protected articlesService = inject(ArticlesService);
+
+  protected articles = this.articlesService.articles;
 }
